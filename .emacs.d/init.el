55c05fcd1db2a30dbf924579543f62c9e7738aee
Grant Austin <gaustin@gmail.com>
2014-01-19 07:20:14 -0700

    autosave/backup dir emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,5 @@
+;; Backup and auto-saves go to ~/.emacs.d/auto-save-list
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
